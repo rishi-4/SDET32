304217e2435c4e7aff1178ae31a82af0ae00da15
user <user@myhp>
2022-04-12 12:19:25 +0530

    commit 2

---
+++ com.crm.comcast.SDET32_GitPractice/src/test/java/com/crm/comcast/SDET32_GitPractice/ClassB.java
@@ -0,0 +1,11 @@
+package com.crm.comcast.SDET32_GitPractice;
+
+import org.testng.annotations.Test;
+
+public class ClassB {
+	@Test
+	public void demoTest() {
+		System.out.println("push 2");
+	}
+
+}
